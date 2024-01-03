b7b68d2dc533fa688f49b3c643a3c892f4370f3f
skim13waketech <56847449+skim13waketech@users.noreply.github.com>
2024-01-03 10:31:37 -0500

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
+// When you scroll below the header page, it apply dark styling.
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
 if(window.scrollY > headerHeight ) {
     header.classList.add('header--dark');
 } else {
     header.classList.remove('header--dark');
 }
+});
+
+// When you scroll below the home section, it becomes transparent.
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+home.style.opacity = 1 - window.scrollY / homeHeight;
 });
